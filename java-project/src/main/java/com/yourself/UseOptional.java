107f8d307c2e8697573a3e423306754981d35d5f
Julien Poulton <julien.poulton@gmail.com>
2017-06-15 15:26:46 +0200

    rewrote TODO comments

--- java-project/src/main/java/com/yourself/UseOptional.java
+++ java-project/src/main/java/com/yourself/UseOptional.java
@@ -20,15 +20,15 @@ public void setDatabase(List<User> database) {
 
 public List<User> database = Database.userDatabase.getList();
 
-public Optional<User> getUserFromDatabase(String name){
-	//TODO return an option of user, where the value is set to the user
-    //who has the name passed in parameters
+public Optional<User> getUserFromDatabase(String name) {
+    //TODO search for the user with the given name in the database, then
+    //return that user within an Optional if found.
 	return Optional.empty();
 }
 
-public Optional<String> getUserNameToUpperCaseFromDatabase(String name){
-	//TODO return an option of user, where the value is set to the user
-    //name in capital letter, who has the name passed in parameters
+public Optional<String> getUserNameToUpperCaseFromDatabase(String name) {
+    //TODO search for the user with the given name in the database, then
+    //return that user's uppercase name within an Optional if found.
 	return Optional.empty();
 }
 
