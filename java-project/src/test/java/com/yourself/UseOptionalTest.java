ef8ec79ca411011f0567a7301917c56fd1513f8c
Julien Poulton <julien.poulton@gmail.com>
2017-06-16 09:25:09 +0200

    clearer unit tests

--- java-project/src/test/java/com/yourself/UseOptionalTest.java
+++ java-project/src/test/java/com/yourself/UseOptionalTest.java
@@ -15,15 +15,34 @@ public class UseOptionalTest {
 
 	@Test
 	public void findUserFromDatabasetest() {
+	    try {
 		Assert.assertTrue("Nico should be in the database.", useOptional.getUserFromDatabase("Nico").isPresent() );
 		Assert.assertEquals("What is Nico's status?", "CTO", useOptional.getUserFromDatabase("Nico").get().getStatus() );
 		Assert.assertFalse("James should not be in the database.", useOptional.getUserFromDatabase("James").isPresent() );
+    } catch (AssertionError ae) {
+        success(false);
+        msg("Oops! 🐞", ae.getMessage());
+    }
 	}
 
 	@Test
 	public void findUserNameToCapitalLetter() {
+	    try {
 		Assert.assertTrue("Uppercase Val should be present.", useOptional.getUserNameToUpperCaseFromDatabase("Val").isPresent());
 		Assert.assertEquals("Uppercase Val.", "VAL", useOptional.getUserNameToUpperCaseFromDatabase("Val").get());
 		Assert.assertFalse("Uppercase Toto should not be present.", useOptional.getUserNameToUpperCaseFromDatabase("Toto").isPresent());
+        } catch (AssertionError ae) {
+            success(false);
+            msg("Oops! 🐞", ae.getMessage());
+        }
 	}
+	
+    private static void success(boolean success) {
+        System.out.println(String.format("TECHIO> success %s", success));
+    }
+    
+
+    private static void msg(String channel, String msg) {
+        System.out.println(String.format("TECHIO> message --channel \"%s\" \"%s\"", channel, msg));
+    }
 }
